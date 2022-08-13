1e5eaf1673345296b572b8656bac48ffd42ce15e
Kieran200 <ann.funtikova06@mail.ru>
2022-08-13 21:26:07 +0300

    1

---
+++ Сlustering_Of_Colors/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Сlustering_Of_Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject_Ranging sr = new Subject_Ranging(); 
+            sr.Ranging(70, 10);
+
+        }
+    }
+}
